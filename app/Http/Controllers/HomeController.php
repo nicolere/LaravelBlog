a7236be7d96cc0762b58c98ba414bfe57187402f
nicolere <nicolas.gauss.1@orange.fr>
2019-04-04 21:28:39 +0200

    Verification base projet

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,9 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
-{
-    
+class HomeController extends Controller 
+{    
     //
     public function index() {
         $posts = \App\Post::orderBy('post_date','desc')->take(3)->get(); //get 3 last posts
